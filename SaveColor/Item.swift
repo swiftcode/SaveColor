e694ef91f086d858a4460a637dc186242969d46c
swiftcode <swiftcode@users.noreply.github.com>
2025-05-23 23:24:22 -0500

    Item + Color
    Create initial Item model and setup extension to Color
    for saving color information to model.
    
    Changes to be committed:
    	modified:   SaveColor/ContentView.swift
    	modified:   SaveColor/Item.swift
    	modified:   SaveColor/SaveColorApp.swift

--- SaveColor/Item.swift
+++ SaveColor/Item.swift
@@ -5,14 +5,51 @@
 //  Created by Michael Campbell on 5/23/25.
 //
 
-import Foundation
-import SwiftData
+import SwiftUI
 
-@Model
-final class Item {
-    var timestamp: Date
+struct Item {
+    var id: UUID
+    var name: String
     
-    init(timestamp: Date) {
-        self.timestamp = timestamp
+    var red: Double = 0.0
+    var green: Double = 0.0
+    var blue: Double = 0.0
+    var alpha: Double = 1.0
+    
+    var color: Color {
+        Color(red: red, green: green, blue: blue, opacity: alpha)
+    }
+    
+    init(id: UUID = UUID(), name: String) {
+        self.id = id
+        self.name = name
     }
 }
+
+extension Item {
+    static var emptyIten: Item {
+        Item(id: UUID(), name: "")
+    }
+}
+
+extension Color {
+    var components: (red: Double, green: Double, blue: Double, alpha: Double) {
+        var red: CGFloat = 0.0
+        var green: CGFloat = 0.0
+        var blue: CGFloat = 0.0
+        var alpha: CGFloat = 0.0
+        
+        #if canImport(UIKIT)
+        let uiColor = UIColor(self)
+        var red: CGFloat = 0.0
+        var green: CGFloat = 0.0
+        var blue: CGFloat = 0.0
+        var alpha: CGFloat = 0.0
+        uiColor.getRed(&red, green: &green, blue: &blue, alpha: &alpha)
+        return (Double(red), Double(green), Double(blue), Double(alpha))
+        #else
+        return (0.0, 0.0, 0.0, 0.0)
+        #endif
+    }
+}
+
